43452f73e4b2b69da1001779c4ab8174f65e32b9
GingerBear <bbbear444@gmail.com>
2017-06-14 19:19:30 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# react-native-barcode-pdf417
+
+React native component for pdf417 barcode. This module is based on https://github.com/bkuzmic/pdf417-js.
+
+
+Pre install:
+Manual linking ART by following http://facebook.github.io/react-native/releases/0.45/docs/linking-libraries-ios.html#manual-linking
+
+Install:
+```
+npm install --save react-native-barcode-pdf417
+```
+
+
+
+Usage:
+
+```js
+import Barcode from "react-native-barcode-pdf417";
+<Barcode text="123124123" width={250} height={100} />
+
+```
---
+++ index.js
@@ -0,0 +1,81 @@
+import React, { Component, PropTypes } from "react";
+import { ART, Text, View, ActivityIndicator, StyleSheet } from "react-native";
+
+const { Group, Shape, Surface } = ART;
+import createPDF417 from "./lib/pdf417-min";
+
+export default class RNPDF417 extends Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired
+  };
+
+  state = {
+    isLoading: true
+  };
+
+  componentDidMount() {
+    requestAnimationFrame(() => {
+      this.setState({
+        isLoading: false
+      });
+    });
+  }
+
+  render() {
+    const { width, height, text } = this.props;
+
+    if (this.state.isLoading) {
+      return (
+        <View style={[S.loadingContainer, { width, height }]}>
+          <ActivityIndicator style={S.loadingIndicator} />
+          <Text>Generating Barcode</Text>
+        </View>
+      );
+    }
+
+    const PDF417 = createPDF417();
+
+    PDF417.init(text);
+    const barcode = PDF417.getBarcodeArray();
+
+    const w = width / barcode.num_cols;
+    const h = height / barcode.num_rows;
+    const shapes = [];
+
+    barcode.bcode.forEach((line, i) => {
+      line.forEach((code, j) => {
+        if (code === "1") {
+          shapes.push(
+            <Shape
+              key={`${i}-${j}`}
+              d={`M ${j * w} ${i * h} h ${w} v ${h} h -${w} L ${j * w} ${i *
+                h}`}
+              fill="#000000"
+            />
+          );
+        }
+      });
+    });
+
+    return (
+      <Surface width={width} height={height}>
+        <Group x={0} y={0}>
+          {shapes}
+        </Group>
+      </Surface>
+    );
+  }
+}
+
+const S = StyleSheet.create({
+  loadingContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  loadingIndicator: {
+    marginRight: 10
+  }
+});
