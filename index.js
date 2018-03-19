5aa91a897fcc824b4989af17557479a12fbb7df7
Corey Pyle <cpyle@mobiquityinc.com>
2018-03-19 09:40:53 -0400

    Update PropTypes import to match React versions v15.5+
    React version 15.5 moved 'PropTypes' out of 'react' and into
    'prop-types'. Also added dependencies for react and prop-types to
    prevent similar issues in the future.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { ART, Text, View, ActivityIndicator, StyleSheet } from "react-native";
 
 const { Group, Shape, Surface } = ART;
